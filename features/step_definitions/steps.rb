0f609a5a149f489ff1782f93ab5155d6f6be10a8
Antonio Lima Goes Junior <ajlima8@gmail.com>
2024-03-21 06:19:04 -0300

    insluindo o visit

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,8 +1,9 @@
 Quando('acesso a página proncipal da Starbugs') do
-     
+     visit 'https://starbugs.vercel.app/'
+     sleep 10
 end
   
   Então('eu quero ver a lista de cafés disoníveis') do
-     
+     puts 'trolei'
 end
   
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-Capybara.config do |config|
+Capybara.configure do |config|
     config.default_driver = :selenium
     config.default_max_wait_time = 5
 end
