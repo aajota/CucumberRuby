5b6368a8274042b79e493a7c57f18a1c0bd69cbf
Antonio Lima Goes Junior <ajlima8@gmail.com>
2024-03-26 05:53:21 -0300

    all

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,9 +1,8 @@
 Quando('acesso a página proncipal da Starbugs') do
      visit 'https://starbugs.vercel.app/'
-     sleep 10
 end
   
   Então('eu quero ver a lista de cafés disoníveis') do
-     puts 'trolei'
+     all('.coffee-item')
 end
-  continuar comentar essa parte
+  
