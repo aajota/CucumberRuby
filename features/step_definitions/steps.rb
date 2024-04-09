0e0e6debdbd2556d04fd972f156186aee9eca122
unknown <ajlima8@gmail.com>
2024-04-09 06:42:59 -0300

    finalizando a automação de café indisponivel

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -12,29 +12,40 @@ Dado('que estou na página principal da Starbugs') do
    end
    
    Dado('que desejo comprar o café {string}') do |product_name|
-     puts product_name
+     @product_name = product_name
    end
    
-   Dado('que esse produto custa R$ {float}') do |product_price|
-     puts product_price
+   Dado('que esse produto custa {string}') do |product_price|
+     @product_price = product_price
    end
-   
-   Dado('que o custo de entrega é de R$ {float}') do |delivery_price|
-     puts delivery_price
+
+   Dado('que o custo de entrega é de {string}') do |delivery_price|
+     @delivery_price = delivery_price
    end
    
    Quando('inicio a compra desse item') do
-     find('.coffee-item', text: 'Expresso Gelado')
+     product = find('.coffee-item', text: @product_name)
+     product.find('.buy-coffee').click
    end
    
    Então('devo ver a página de checkout com os detalhes do croduto') do
+     product_title = find('.item-details h1')
+     expect(product_title.text).to eql @product_name
 
+     sub_price  = find('.subtotal .sub-price')
+     expect(sub_price.text).to eql @product_price
+
+     delivery = find('.delivery-price')
+     expect(delivery.text).to eql @delivery_price
    end
    
-   Então('o valor total da compra deve ser de R$ {float}') do |float|
+   Então('o valor total da compra deve ser de {string}') do |total_price|
+     price = find('.total-price')
+     expect(price.text).to eql total_price
 
    end
    
    Então('devo ver um popup informando que o produto está indisponível') do      
-
+     popup = find('.swal2-html-container')
+     expect(popup.text).to eql 'Produto indisponível'
    end
