6f88fa665bf9170f2eef4c2998286feed9fa430f
unknown <ajlima8@gmail.com>
2024-04-11 15:38:59 -0300

    refaturando

--- features/catalogo.feature
+++ features/catalogo.feature
@@ -9,18 +9,19 @@ Cenário: Ancessar o catálogo de cafés na página principal
     Quando acesso a página proncipal da Starbugs
     Então eu quero ver a lista de cafés disoníveis 
 
-
+@temp
 Cenário: Iniciar a compra de um café
 
     Dado que estou na página principal da Starbugs
-        E que desejo comprar o café "Expresso Gelado"
-        E que esse produto custa "R$ 9,99"
-        E que o custo de entrega é de "R$ 10,00"
+        E que desejo comprar o seguinte produto:
+        |name|Expresso Gelado|
+        |price|R$ 9,99|
+        |delivery|R$ 10,00|
     Quando inicio a compra desse item
     Então devo ver a página de checkout com os detalhes do croduto
-        E o valor total da compra deve ser de "R$ 19,00"  
+        E o valor total da compra deve ser de "R$ 19,99"  
       
-@temp
+
 Cenário: Café indisponível      
 
     Dado que estou na página principal da Starbugs
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -10,42 +10,35 @@ end
 Dado('que estou na página principal da Starbugs') do
      visit 'https://starbugs.vercel.app/'
    end
-   
-   Dado('que desejo comprar o café {string}') do |product_name|
-     @product_name = product_name
-   end
-   
-   Dado('que esse produto custa {string}') do |product_price|
-     @product_price = product_price
-   end
 
-   Dado('que o custo de entrega é de {string}') do |delivery_price|
-     @delivery_price = delivery_price
-   end
+Dado('que desejo comprar o seguinte produto:') do |table|
+    @product = table.rows_hash
+
+  end
    
-   Quando('inicio a compra desse item') do
-     product = find('.coffee-item', text: @product_name)
+Quando('inicio a compra desse item') do
+     product = find('.coffee-item', text: @product[:name])
      product.find('.buy-coffee').click
    end
    
-   Então('devo ver a página de checkout com os detalhes do croduto') do
+Então('devo ver a página de checkout com os detalhes do croduto') do
      product_title = find('.item-details h1')
-     expect(product_title.text).to eql @product_name
+     expect(product_title.text).to eql @product[:name]
 
      sub_price  = find('.subtotal .sub-price')
-     expect(sub_price.text).to eql @product_price
+     expect(sub_price.text).to eql @product[:price]
 
      delivery = find('.delivery-price')
-     expect(delivery.text).to eql @delivery_price
+     expect(delivery.text).to eql @product[:delivery]
    end
    
-   Então('o valor total da compra deve ser de {string}') do |total_price|
+Então('o valor total da compra deve ser de {string}') do |total_price|
      price = find('.total-price')
      expect(price.text).to eql total_price
 
    end
    
-   Então('devo ver um popup informando que o produto está indisponível') do      
+Então('devo ver um popup informando que o produto está indisponível') do      
      popup = find('.swal2-html-container')
      expect(popup.text).to eql 'Produto indisponível'
    end
