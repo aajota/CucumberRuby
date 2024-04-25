1d206c733fd0b4d20f31060de302504add499681
unknown <ajlima8@gmail.com>
2024-04-25 16:02:59 -0300

    zipcode

--- features/step_definitions/pedido_steps.rb
+++ features/step_definitions/pedido_steps.rb
@@ -1,9 +1,10 @@
 Dado('que inciei a compra do item {string}') do |product_name|
-    @home.buy(@product[:name])
+    @home.buy(product_name)
   end
   
-  Quando('faço a busca do seguinte CEP: {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Quando('faço a busca do seguinte CEP: {string}') do |zipcode|
+    find('input[name=cep]').set(zipcode)
+    click_on 'Buscar CEP'
   end
   
   Quando('informo os demais dados do endereço:') do |table|
