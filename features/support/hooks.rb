30c2f38e7ebb32c49df7cdba88518bd2fe2d4b1b
unknown <ajlima8@gmail.com>
2024-06-19 09:49:37 -0300

    hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,11 +1,18 @@
 
 require_relative './pages/home.rb'
 require_relative './pages/checkout.rb'
+require_relative './pages/order.rb'
 require_relative './pages/components.rb'
 
 Before do
     @home = HomePage.new
     @checkout = CheckoutPages.new
     @popup = Popup.new
+    @order = OrderPage.new
+    @popup = Popup.new
+
+end
+
+After do
 
 end
