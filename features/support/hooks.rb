ee6cc1a6bac40710c323b3924b933070da877946
unknown <ajlima8@gmail.com>
2024-06-19 10:27:56 -0300

    attach

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,18 +1,20 @@
 
 require_relative './pages/home.rb'
 require_relative './pages/checkout.rb'
-require_relative './pages/order.rb'
+#require_relative './pages/order.rb'
 require_relative './pages/components.rb'
 
 Before do
     @home = HomePage.new
     @checkout = CheckoutPages.new
     @popup = Popup.new
-    @order = OrderPage.new
-    @popup = Popup.new
+    #@order = OrderPage.new
+    #@popup = Popup.new
 
 end
 
 After do
+    screenshot = page.save_screenshot("logs/screenshots/temp.png")
+    attach(screenshot, "image/png", "Screenshot")
 
 end
